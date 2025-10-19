2cb96c219c77b45cde6d450700f9eee607dfb487
Elias Aebi <eyelash@users.noreply.github.com>
2025-10-19 13:09:57 +0200

    move the collectors up

--- parser.cpp
+++ parser.cpp
@@ -3,35 +3,6 @@
 
 using namespace parser;
 
-constexpr auto whitespace_char = choice(' ', '\t', '\n', '\r');
-
-constexpr auto numeric_char = range('0', '9');
-
-constexpr auto alphabetic_char = choice(range('a', 'z'), range('A', 'Z'), '_');
-
-constexpr auto alphanumeric_char = choice(alphabetic_char, numeric_char);
-
-constexpr auto comment = choice(
-	sequence("//", zero_or_more(sequence(not_("\n"), any_char()))),
-	sequence("/*", zero_or_more(sequence(not_("*/"), any_char())), expect("*/"))
-);
-
-constexpr auto whitespace = ignore(sequence(
-	zero_or_more(whitespace_char),
-	zero_or_more(sequence(
-		comment,
-		zero_or_more(whitespace_char)
-	))
-));
-
-constexpr auto keyword(const char* s) {
-	return sequence(ignore(s), not_(alphanumeric_char));
-}
-
-template <class P> constexpr auto operator_(P p) {
-	return sequence(whitespace, ignore(p), whitespace);
-}
-
 class NameCollector {
 	std::string name;
 public:
@@ -42,8 +13,6 @@ public:
 		callback.push(std::move(name));
 	}
 };
-
-constexpr auto identifier = collect<NameCollector>(sequence(alphabetic_char, zero_or_more(alphanumeric_char)));
 
 class IntCollector {
 	std::int32_t value = 0;
@@ -57,8 +26,6 @@ public:
 	}
 };
 
-constexpr auto int_literal = collect<IntCollector>(one_or_more(numeric_char));
-
 class ExpressionCollector {
 	Reference<Expression> expression;
 public:
@@ -71,35 +38,6 @@ public:
 	template <class C> void retrieve(const C& callback) {
 		callback.push(std::move(expression));
 	}
-};
-
-template <BinaryOperation operation> class OperationMapper {
-public:
-	template <class C, class... A> static void map(const C& callback, A&&... a) {
-		callback.push(std::forward<A>(a)..., operation);
-	}
-};
-
-struct expression {
-	static constexpr auto parser = pratt<ExpressionCollector>(
-		pratt_level(
-			infix_ltr<OperationMapper<BinaryOperation::ADD>>(operator_('+')),
-			infix_ltr<OperationMapper<BinaryOperation::SUB>>(operator_('-'))
-		),
-		pratt_level(
-			infix_ltr<OperationMapper<BinaryOperation::MUL>>(operator_('*')),
-			infix_ltr<OperationMapper<BinaryOperation::DIV>>(operator_('/')),
-			infix_ltr<OperationMapper<BinaryOperation::REM>>(operator_('%'))
-		),
-		pratt_level(
-			terminal(choice(
-				sequence(ignore('('), whitespace, reference<expression>(), whitespace, expect(")")),
-				int_literal,
-				ignore(identifier),
-				error("expected an expression")
-			))
-		)
-	);
 };
 
 class StatementCollector {
@@ -176,6 +114,68 @@ public:
 	template <class C> void retrieve(const C& callback) {
 		callback.push(Program(std::move(functions)));
 	}
+};
+
+constexpr auto whitespace_char = choice(' ', '\t', '\n', '\r');
+
+constexpr auto numeric_char = range('0', '9');
+
+constexpr auto alphabetic_char = choice(range('a', 'z'), range('A', 'Z'), '_');
+
+constexpr auto alphanumeric_char = choice(alphabetic_char, numeric_char);
+
+constexpr auto comment = choice(
+	sequence("//", zero_or_more(sequence(not_("\n"), any_char()))),
+	sequence("/*", zero_or_more(sequence(not_("*/"), any_char())), expect("*/"))
+);
+
+constexpr auto whitespace = ignore(sequence(
+	zero_or_more(whitespace_char),
+	zero_or_more(sequence(
+		comment,
+		zero_or_more(whitespace_char)
+	))
+));
+
+constexpr auto keyword(const char* s) {
+	return sequence(ignore(s), not_(alphanumeric_char));
+}
+
+template <class P> constexpr auto operator_(P p) {
+	return sequence(whitespace, ignore(p), whitespace);
+}
+
+constexpr auto identifier = collect<NameCollector>(sequence(alphabetic_char, zero_or_more(alphanumeric_char)));
+
+constexpr auto int_literal = collect<IntCollector>(one_or_more(numeric_char));
+
+template <BinaryOperation operation> class OperationMapper {
+public:
+	template <class C, class... A> static void map(const C& callback, A&&... a) {
+		callback.push(std::forward<A>(a)..., operation);
+	}
+};
+
+struct expression {
+	static constexpr auto parser = pratt<ExpressionCollector>(
+		pratt_level(
+			infix_ltr<OperationMapper<BinaryOperation::ADD>>(operator_('+')),
+			infix_ltr<OperationMapper<BinaryOperation::SUB>>(operator_('-'))
+		),
+		pratt_level(
+			infix_ltr<OperationMapper<BinaryOperation::MUL>>(operator_('*')),
+			infix_ltr<OperationMapper<BinaryOperation::DIV>>(operator_('/')),
+			infix_ltr<OperationMapper<BinaryOperation::REM>>(operator_('%'))
+		),
+		pratt_level(
+			terminal(choice(
+				sequence(ignore('('), whitespace, reference<expression>(), whitespace, expect(")")),
+				int_literal,
+				ignore(identifier),
+				error("expected an expression")
+			))
+		)
+	);
 };
 
 struct block;
